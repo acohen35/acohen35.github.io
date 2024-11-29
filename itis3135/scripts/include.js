47d4314873ee699f65d56d1bf2c138cc872e53db
Aaron Cohen <acohen35@uncc.edu>
2024-11-29 11:55:54 -0500

    fixed include.js

--- itis3135/scripts/include.js
+++ itis3135/scripts/include.js
@@ -6,6 +6,12 @@ var HTMLInclude = function() {
         xhr.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 element.outerHTML = this.responseText;
+                if (url.includes('footer')) {
+                    document.getElementById('validation_link_html').setAttribute('href', 
+                        'https://validator.w3.org/check?uri=' + location.href);
+                    document.getElementById('validation_link_css').setAttribute('href', 
+                        'https://jigsaw.w3.org/css-validator/check?uri=' + location.href);
+                }
             }
         };
         xhr.open('GET', url, true);
